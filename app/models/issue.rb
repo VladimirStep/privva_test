34a8e5f2596e186ad5e5b1e190b68427e1996df8
Vladimir HP250 <vladimir_invest@inbox.ru>
2018-08-23 07:42:25 -0400

    Add sorting by sort_order param

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,12 @@ class Issue < ApplicationRecord
   belongs_to :reporter, class_name: 'User', foreign_key: :reporter_id
   belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
 
+  scope :filter, ->(params) {
+    relation = left_outer_joins(:reporter, :assignee).where nil
+    relation = relation.order(created_at: params[:sort_order].to_sym) if %i[asc desc].include?(params[:sort_order].to_sym)
+    relation
+  }
+
   enum status: %i[active archived]
 
   self.per_page = 10
