7ca0679647c6b8015dbba1285b59551b8ec8dad3
Vladimir HP250 <vladimir_invest@inbox.ru>
2018-08-23 05:59:43 -0400

    Add associations. Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+reporters = []
+
+3.times do |index|
+  index += 1
+  reporter = User.create(
+    name: "Reporter_#{index}",
+    email: "reporter_#{index}@example.com"
+  )
+  reporters << reporter
+end
+
+assignees = []
+
+2.times do |index|
+  index += 1
+  assignee = User.create(
+    name: "Assignee_#{index}",
+    email: "assignee_#{index}@example.com"
+  )
+  assignees << assignee
+end
+
+statuses = %w[active archived]
+
+reporters.each do |reporter|
+  5.times do |index|
+    index += 1
+    Issue.create(
+      summary: "Summary of issue #{index}",
+      status: statuses.sample,
+      reporter: reporter,
+      assignee: assignees.sample
+    )
+  end
+end
