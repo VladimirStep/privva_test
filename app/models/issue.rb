3f22f2a1efe80e866454588b4215f4622f9bc7fb
Vladimir HP250 <vladimir_invest@inbox.ru>
2018-08-23 08:45:52 -0400

    Add serach by reporter name and assignee name

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,7 +3,9 @@ class Issue < ApplicationRecord
   belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
 
   scope :filter, ->(params) {
-    relation = left_outer_joins(:reporter, :assignee).where nil
+    relation = where nil
+    relation = relation.joins('INNER JOIN "users" "reporters" ON "reporters"."id" = "issues"."reporter_id"').where(reporters: { name: params[:reporter_name] }) if params[:reporter_name].present?
+    relation = relation.joins('INNER JOIN "users" "assignees" ON "assignees"."id" = "issues"."assignee_id"').where(assignees: { name: params[:assignee_name] }) if params[:assignee_name].present?
     relation = relation.order(created_at: params[:sort_order].to_sym) if %i[asc desc].include?(params[:sort_order].to_sym)
     relation
   }
