935c0269305bde3dbdd1c48072dbf3aaa36f0a90
Vladimir HP250 <vladimir_invest@inbox.ru>
2018-08-23 06:52:03 -0400

    Add pagination

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class IssuesController < ApplicationController
       def index
-        issues = Issue.all
+        issues = Issue.paginate(page: params[:page], per_page: params[:page_size])
         render json: issues
       end
     end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,6 @@ class Issue < ApplicationRecord
   belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id
 
   enum status: %i[active archived]
+
+  self.per_page = 10
 end
