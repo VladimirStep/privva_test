ff0ceb92b3679f46592cf258b95969e406b6b320
Vladimir HP250 <vladimir_invest@inbox.ru>
2018-08-23 08:55:43 -0400

    Add search by summary

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,6 +4,7 @@ class Issue < ApplicationRecord
 
   scope :filter, ->(params) {
     relation = where nil
+    relation = relation.where('"summary" LIKE ?', "%#{params[:issue_summary]}%") if params[:issue_summary].present?
     relation = relation.joins('INNER JOIN "users" "reporters" ON "reporters"."id" = "issues"."reporter_id"').where(reporters: { name: params[:reporter_name] }) if params[:reporter_name].present?
     relation = relation.joins('INNER JOIN "users" "assignees" ON "assignees"."id" = "issues"."assignee_id"').where(assignees: { name: params[:assignee_name] }) if params[:assignee_name].present?
     relation = relation.order(created_at: params[:sort_order].to_sym) if %i[asc desc].include?(params[:sort_order].to_sym)
